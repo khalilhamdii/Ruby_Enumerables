22bf9c18695e8422f581d14abdc2e6b82f585a14
khalilhamdii <khaalil.hamdi@gmail.com>
2020-09-17 11:46:50 +0100

    Add my_all/my_any/my_none functions

--- main.rb
+++ main.rb
@@ -28,12 +28,88 @@ module Enumerable
     selected_arr = []
     my_each do |item|
       next unless yield(item)
-​
       selected_arr.push(item)
     end
-
   selected_arr
   end
+
+   # 4 - my_all? code
+
+   def my_all?(condition=false)
+
+    my_each do |item|
+      if block_given? 
+        unless yield(item) 
+          return false
+        end
+      elsif condition.class == Class
+        unless item.is_a?(condition)
+          return false
+        end
+      elsif condition.class == Regexp
+        unless item.match(condition)
+          return false
+        end
+      elsif !item
+        return false
+      end
+    end
+      true
+    end
+
+  # 5 - my_any? code 
+
+  def my_any?(condition=false)
+
+    my_each do |item|
+      if block_given? 
+        if yield(item) 
+          return true
+        end
+      elsif condition.class == Class
+        if item.is_a?(condition)
+          return true
+        end
+      elsif condition.class == Regexp
+        if item.match(condition)
+          return true
+        end
+      elsif item
+        return true
+      end
+    end
+      false
+    end
+
+     # 6 - my_none? code  
+
+  def my_none?(condition=false)
+
+    my_each do |item|
+      if block_given? 
+        if yield(item) 
+          return false
+        end
+      elsif condition.class == Class
+        if item.is_a?(condition)
+          return false
+        end
+      elsif condition.class == Regexp
+        if item.match(condition)
+          return false
+        end
+      elsif item
+        return false
+      end
+    end
+      true
+    end
+
+
+
+
+
+
 end
 
 
@@ -48,8 +124,34 @@ end
 #   }
 
 # 3 *** my_select test
-​
- print [1, 2, 3, 4, 5].my_select { |num| num.even? }
-​
 
+# print [1, 2, 3, 4, 5].my_select { |num| num.even?}
 
+# # 4 *** my_all? test
+
+# puts %w[ant bear cat].my_all? { |word| word.length >= 3 }
+# puts %w[ant bear cat].my_all? { |word| word.length >= 4 }
+# puts %w[ant bear cat].my_all?(/t/)
+# puts [1, 2i, 3.14].my_all?(Numeric)
+# puts [nil, true, 99].my_all?
+# puts [].my_all?
+
+# # 5 *** my_any? test
+
+# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+# puts %w[ant bear cat].my_any?(/d/)                        #=> false
+# puts [nil, true, 99].my_any?(Integer)                     #=> true
+# puts [nil, true, 99].my_any?                              #=> true
+# puts [].my_any?                                           #=> false
+
+# # 6 *** my_none? test
+
+# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# puts %w{ant bear cat}.my_none?(/d/)                        #=> true
+# puts [1, 3.14, 42].my_none?(Float)                         #=> false
+# puts [].my_none?                                           #=> true
+# puts [nil].my_none?                                        #=> true
+# puts [nil, false].my_none?                                 #=> true
+# puts [nil, false, true].my_none?                           #=> false
